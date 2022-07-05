c721837eb909b3811913a0bd88b358318a61a529
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-07-05 20:37:42 +0700

    test

--- custom_components/boy_notify_line/notify.py
+++ custom_components/boy_notify_line/notify.py
@@ -45,7 +45,6 @@ def get_service(hass, config, discovery_info=None):
     """Get the Line notification service."""
     access_token = config.get(CONF_ACCESS_TOKEN )
     if access_token is None:
-        _LOGGER.error("No access token found in configuration")
         return LineNotificationService(access_token)
     else:
         return LineNotificationService()
